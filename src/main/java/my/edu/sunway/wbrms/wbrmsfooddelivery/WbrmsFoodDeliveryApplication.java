f602feb407f9d106aed845678b5aeebbe332c062
vrnsky <vrnsky@protomail.ch>
2023-10-07 18:00:23 +0800

    Initial commit with a stub of implementation

---
+++ src/main/java/my/edu/sunway/wbrms/wbrmsfooddelivery/WbrmsFoodDeliveryApplication.java
@@ -0,0 +1,13 @@
+package my.edu.sunway.wbrms.wbrmsfooddelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WbrmsFoodDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WbrmsFoodDeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/my/edu/sunway/wbrms/wbrmsfooddelivery/WbrmsFoodDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package my.edu.sunway.wbrms.wbrmsfooddelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WbrmsFoodDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
