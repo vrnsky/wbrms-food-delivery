99dbca2efd9e58dc7b512a529191e11f548c7255
vrnsky <vrnsky@protonmail.ch>
2023-10-30 20:03:28 +0800

    CP-005. Add more APIs
    refs: CP-005

--- src/main/java/my/edu/sunway/wbrms/wbrmsfooddelivery/api/DeliveryController.java
+++ src/main/java/my/edu/sunway/wbrms/wbrmsfooddelivery/api/DeliveryController.java
@@ -3,9 +3,7 @@ package my.edu.sunway.wbrms.wbrmsfooddelivery.api;
 import lombok.RequiredArgsConstructor;
 import my.edu.sunway.wbrms.wbrmsfooddelivery.dto.Delivery;
 import my.edu.sunway.wbrms.wbrmsfooddelivery.service.DeliveryService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
@@ -21,4 +19,20 @@ public class DeliveryController {
     public Mono<List<Delivery>> list() {
         return Mono.just(deliveryService.getDefaultDeliveries());
     }
+
+    @PostMapping("/create")
+    public Mono<Delivery> create(@RequestBody Delivery delivery) {
+        return Mono.empty();
+    }
+
+    @PutMapping("/update/{deliveryId}")
+    public Mono<Delivery> update(@PathVariable String deliveryId, @RequestBody Delivery delivery) {
+        return Mono.empty();
+    }
+
+    @DeleteMapping("/delete/{deliveryId}")
+    public Mono<?> delete(@PathVariable String deliveryId) {
+        return Mono.empty();
+    }
+
 }
